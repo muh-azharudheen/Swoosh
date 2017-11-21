ffb53190185e5d0724e2170b85bb26a737433e3b
Mac <mac@Macs-MacBook-Pro.local>
2017-11-21 23:16:49 +0400

    completed Swoosh

--- app/src/main/java/com/azharudhen/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/azharudhen/swoosh/Controller/LeagueActivity.kt
@@ -2,6 +2,7 @@ package com.azharudhen.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.azharudhen.swoosh.Model.Player
@@ -17,6 +18,19 @@ class LeagueActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+
+        }
     }
 
     fun onMensClicked(view: View){
